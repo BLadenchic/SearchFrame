0bf137466fac51bc07cd6883788d37447a81033a
Trelinator <abraKavadra@gmail.com>
2021-09-19 07:29:02 +0300

    start

--- check.php
+++ check.php
@@ -2,18 +2,16 @@
 
 $login = filter_var(trim($_POST["login"]),FILTER_SANITIZE_STRING);
 $password = filter_var(trim($_POST["password"]),FILTER_SANITIZE_STRING);
-$link = mysqli_connect('localhost', 'root', '','testing');
-					$sql =  'select *
-							 from users
-							 where login="'.$login.'" AND password="'.$password.'"';
+$link = mysqli_connect('localhost', 'mysql', '','information');
+		$sql =  "SELECT * FROM `users`
+		WHERE `login` = '".$login."' AND `password`='".$password."'";
 					$result=mysqli_query($link, $sql) or die("Ошибка ".mysqli_error($link));
 					$user=$result->fetch_assoc();
 if(!$user)
 {
-    header("Location:./");
+	header("Location:./");
 	exit;
 }
-print_r($user);
 setcookie('user',$user['id_user'],time()+3600,'/');
 setcookie('user_type',$user['id_type'],time()+3600,'/');
 mysqli_close($link);
--- index.php
+++ index.php
@@ -2,41 +2,35 @@
 <head>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="./style.css">
-<script src='ERROR.js'></script>
+<?php if(isset($_COOKIE['user']))
+header("Location:./Profil");?>
 <title>Тестирование кадров</title>
 </head>
 <body>
-<?php 
-if(!isset($_COOKIE['user'])):
-?>
-
-
 <div class="form">
-<div class='login'>
-<div class="promo"><h5>Тестирование кадров</h5></div>
-<form action="check.php" method="POST">
-	<div class='login-forma'>
-		<div class='login-forma-name'>
-			<a>Логин: </a>
-			<input type='text' name='login' id='name'></input>
-		</div>
-		<div class='login-forma-password'>
-		<a>Пароль: </a>
-			<input type='password' name='password' id='password'></input>
+	<div class='login'>
+		<div class="promo"><h5>Тестирование кадров</h5></div>
+			<form action="check.php" method="POST">
+				<div class='login-forma'>
+					<div class='login-forma-name'>
+						<a>Логин: </a>
+						<input type='text' name='login' id='name'></input>
+					</div>
+					<div class='login-forma-password'>
+						<a>Пароль: </a>
+						<input type='password' name='password' id='password'></input>
+					</div>
+				</div>
+				<div>
+					<input type='submit' value='Войти' class="but"></input>
+				</div>
+				<div class="login-forma-button">
+					<a href="Registration">Зарегистрироватся</a>
+					<a href="Reset">Забыл пароль</a>
+				</div>
+			</form>
 		</div>
 	</div>
-	<div>
-	<input type='submit' value='Войти' class="but">
-	</input>
-	</div>
-	</form>
 </div>
-</div>
-
-<?php 
-else:
-header("Location:/Profil");
-endif;
-?>
 </body>
 </html>
